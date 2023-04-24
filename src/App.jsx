ab44a9713d0aeec454d1dc655f6d481390b5f977
Gustav Wahlström <gustav.wahlstrom@outlook.com>
2023-04-24 16:00:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import styles from "./App.module.scss";
+import Nav from "./components/nav";
+
+function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import styles from "./Nav.module.scss";
+
+function Nav() {
+  return (
+    <>
+      <div className={styles.box}>
+        <img src="./GWdocslogo.png" alt="" />
+      </div>
+    </>
+  );
+}
+
+export default Nav;
